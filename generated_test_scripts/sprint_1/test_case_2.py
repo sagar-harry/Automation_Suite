c814ee41791abd934910ae0f730269d256e6953c
sagar-harry <10vidyasagarkonni@gmail.com>
2025-02-25 06:27:30 +0000

    Push test scripts

---
+++ generated_test_scripts/sprint_1/test_case_2.py
@@ -0,0 +1,65 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+import sys
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def login(self, username, password):
+        self.driver.find_element(By.XPATH, '//*[@id="user-name"]').send_keys(username)
+        self.driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(password)
+        time.sleep(3)
+        self.driver.find_element(By.XPATH, '//*[@id="login-button"]').click()
+
+try:
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--incognito')
+    options.add_argument('--disable-notifications')
+    options.add_argument('--disable-features=NetworkService')
+
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+
+    driver.get("https://saucedemo.com/")
+    time.sleep(5)
+
+    login_page = LoginPage(driver)
+    login_page.login("standard_user", "secret_sauce")
+
+    time.sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-bike-light"]').click()
+    time.sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
+    time.sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a').click()
+    time.sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="checkout"]').click()
+    time.sleep(3)
+    
+    driver.find_element(By.XPATH, '//*[@id="first-name"]').send_keys("somename")
+    time.sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="last-name"]').send_keys("lastname")
+    time.sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="postal-code"]').send_keys("123456")
+    time.sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="continue"]').click()
+    time.sleep(3)
+
+    payment_info_visible = driver.find_element(By.XPATH, '//*[@id="checkout_summary_container"]/div/div[2]/div[1]').is_displayed()
+
+    if payment_info_visible:
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+except Exception as e:
+    print(f"Test case failed due to exception: {e}")
+    sys.exit(1)
+finally:
+    driver.quit()
