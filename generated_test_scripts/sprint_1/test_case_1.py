c4e7fbe89906d4369d57fc62c1a537bd6536c2ca
sagar-harry <10vidyasagarkonni@gmail.com>
2025-02-25 08:03:21 +0000

    Push test scripts

--- generated_test_scripts/sprint_1/test_case_1.py
+++ generated_test_scripts/sprint_1/test_case_1.py
@@ -2,72 +2,104 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import sys
 
-def login(driver, username, password):
-    driver.find_element(By.XPATH, '//*[@id="user-name"]').send_keys(username)
-    driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(password)
-    driver.find_element(By.XPATH, '//*[@id="login-button"]').click()
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
 
-def main():
+    def login(self, username, password):
+        user_name_input = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="user-name"]')))
+        password_input = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="password"]')))
+        login_button = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="login-button"]')))
+       
+        user_name_input.send_keys(username)
+        time.sleep(3)
+        password_input.send_keys(password)
+        time.sleep(3)
+        login_button.click()
+        time.sleep(3)
+
+def test_ui_scenario():
     options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-notifications')
-    options.add_argument('--incognito')
-    options.add_argument('--disable-features=NetworkService')
-    options.add_argument('--window-size=1920,1080')
+    options.add_argument("--headless")
+    options.add_argument("--disable-notifications")
+    options.add_argument("--incognito")
+    options.add_argument("--disable-features=NetworkService")
+    
+    driver = webdriver.Chrome(options=options)
 
     try:
-        driver = webdriver.Chrome(options=options)
         driver.get("https://saucedemo.com/")
         time.sleep(5)
         driver.maximize_window()
         time.sleep(3)
 
-        login(driver, "standard_user", "secret_sauce")
-        
-        time.sleep(3)        
-        bike_light_button = driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-bike-light"]')
-        fleece_jacket_button = driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-fleece-jacket"]')
-        
+        login_page = LoginPage(driver)
+        login_page.login("standard_user", "secret_sauce")
+
+        # Add 'Bike Light' to the cart
+        bike_light_button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="add-to-cart-sauce-labs-bike-light"]')))
         bike_light_button.click()
         time.sleep(3)
+
+        # Add 'Fleece Jacket' to the cart
+        fleece_jacket_button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="add-to-cart-sauce-labs-fleece-jacket"]')))
         fleece_jacket_button.click()
         time.sleep(3)
+
+        # Verify cart badge displays '2'
+        cart_badge = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="shopping_cart_container"]/a/span')))
+        assert cart_badge.text == '2', "Cart badge count is incorrect"
         
-        cart_badge = driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a/span').text
-        if cart_badge != '2':
-            sys.exit(1)
-            
-        remove_bike_light = driver.find_element(By.XPATH, '//*[@id="remove-sauce-labs-bike-light"]')
-        remove_fleece_jacket = driver.find_element(By.XPATH, '//*[@id="remove-sauce-labs-fleece-jacket"]')
-        
-        remove_bike_light.click()
-        time.sleep(3)
-        remove_fleece_jacket.click()
-        time.sleep(3)
-        
-        cart_badge = driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a/span').text
-        if cart_badge != '':
-            sys.exit(1)
-        
-        add_bolt_tshirt = driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-bolt-t-shirt"]')
-        
-        add_bolt_tshirt.click()
+        # Remove 'Bike Light'
+        remove_bike_light_button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="remove-sauce-labs-bike-light"]')))
+        remove_bike_light_button.click()
         time.sleep(3)
 
-        cart_badge = driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a/span').text
-        if cart_badge != '1':
-            sys.exit(1)
+        # Remove 'Fleece Jacket'
+        remove_fleece_jacket_button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="remove-sauce-labs-fleece-jacket"]')))
+        remove_fleece_jacket_button.click()
+        time.sleep(3)
+
+        # Verify cart count element doesn't exist
+        try:
+            driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a/span')
+            cart_badge_exists = True
+        except:
+            cart_badge_exists = False
         
-        sys.exit(0)
-    except Exception as e:
-        print("Test failed:", e)
-        sys.exit(1)
-    finally:
+        assert not cart_badge_exists, "Cart badge element exists when it should not"
+
+        # Add 'Bolt T-Shirt' to the cart
+        bolt_tshirt_button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="add-to-cart-sauce-labs-bolt-t-shirt"]')))
+        bolt_tshirt_button.click()
+        time.sleep(3)
+
+        # Verify cart badge displays '1'
+        cart_badge = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="shopping_cart_container"]/a/span')))
+        assert cart_badge.text == '1', "Cart badge count is incorrect after adding Bolt T-Shirt"
+
+        # Close browser and pass the test
         driver.quit()
+        sys.exit(0)
+    
+    except Exception as e:
+        print(e)
+        driver.quit()
+        sys.exit(1)
 
-if __name__ == "__main__":
-    main()
+test_ui_scenario()
