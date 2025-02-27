b71a4f0cb5f0ad8b4ca97eaf534c04d8d84a8985
sagar-harry <10vidyasagarkonni@gmail.com>
2025-02-27 12:18:40 +0000

    Push test scripts

--- generated_test_scripts/sprint_1/test_case_2.py
+++ generated_test_scripts/sprint_1/test_case_2.py
@@ -7,78 +7,90 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
-
+        
     def login(self, username, password):
-        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="user-name"]')))
         self.driver.find_element(By.XPATH, '//*[@id="user-name"]').send_keys(username)
-        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="password"]')))
         self.driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(password)
-        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="login-button"]')))
+        time.sleep(3)
         self.driver.find_element(By.XPATH, '//*[@id="login-button"]').click()
 
-def run_test():
-    options = Options()
-    options.add_argument('--incognito')
-    options.add_argument('--disable-notifications')
-    options.add_argument('--disable-features=NetworkService')
 
-    driver = webdriver.Chrome(options=options)
-
+def main():
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument("--disable-popup-blocking")
+    chrome_options.add_argument("--incognito")
+    chrome_options.add_argument("--disable-features=NetworkService")
+    
+    driver = webdriver.Chrome(options=chrome_options)
+    
     try:
-        driver.maximize_window()
-        driver.get('https://saucedemo.com/')
+        driver.get("https://saucedemo.com/")
         time.sleep(5)
-
+        driver.maximize_window()
+        
         login_page = LoginPage(driver)
-        login_page.login('standard_user', 'secret_sauce')
+        login_page.login("standard_user", "secret_sauce")
+        
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="add-to-cart-sauce-labs-bike-light"]'))
+        ).click()
         time.sleep(3)
-
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="add-to-cart-sauce-labs-bike-light"]')))
-        driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-bike-light"]').click()
+        
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="add-to-cart-sauce-labs-bolt-t-shirt"]'))
+        ).click()
         time.sleep(3)
-
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="add-to-cart-sauce-labs-bolt-t-shirt"]')))
-        driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
+        
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="shopping_cart_container"]/a'))
+        ).click()
         time.sleep(3)
-
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="shopping_cart_container"]/a')))
-        driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a').click()
+        
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="checkout"]'))
+        ).click()
         time.sleep(3)
-
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="checkout"]')))
-        driver.find_element(By.XPATH, '//*[@id="checkout"]').click()
+        
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="first-name"]'))
+        ).send_keys("somename")
         time.sleep(3)
-
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="first-name"]')))
-        driver.find_element(By.XPATH, '//*[@id="first-name"]').send_keys('somename')
+        
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="last-name"]'))
+        ).send_keys("lastname")
         time.sleep(3)
-
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="last-name"]')))
-        driver.find_element(By.XPATH, '//*[@id="last-name"]').send_keys('lastname')
+        
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="postal-code"]'))
+        ).send_keys("123456")
         time.sleep(3)
-
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="postal-code"]')))
-        driver.find_element(By.XPATH, '//*[@id="postal-code"]').send_keys('123456')
+        
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="continue"]'))
+        ).click()
         time.sleep(3)
-
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="continue"]')))
-        driver.find_element(By.XPATH, '//*[@id="continue"]').click()
-        time.sleep(3)
-
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="checkout_summary_container"]/div/div[2]/div[1]')))
-        screenshot_path = r'C:\Users\Administrator\Desktop\QE_COE\automated_pipeline_2\captured_screenshots\\screenshot.png'
-        driver.save_screenshot(screenshot_path)
+        
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="checkout_summary_container"]/div/div[2]/div[1]'))
+        )
+        
+        driver.save_screenshot(r"C:\Users\Administrator\Desktop\QE_COE\automated_pipeline_2\captured_screenshots\screenshot.png")
         sys.exit(0)
 
     except Exception as e:
-        screenshot_path = r'C:\Users\Administrator\Desktop\QE_COE\automated_pipeline_2\captured_screenshots\\screenshot.png'
-        driver.save_screenshot(screenshot_path)
+        driver.save_screenshot(r"C:\Users\Administrator\Desktop\QE_COE\automated_pipeline_2\captured_screenshots\screenshot.png")
+        print(str(e))
         sys.exit(1)
 
     finally:
         driver.quit()
 
-run_test()
+
+if __name__ == "__main__":
+    main()
