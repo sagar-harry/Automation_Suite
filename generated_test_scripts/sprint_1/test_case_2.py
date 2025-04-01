5bc7f28f163030ab44d9018a39570dbd68b9f570
sagar-harry <10vidyasagarkonni@gmail.com>
2025-04-01 13:30:11 +0000

    Push test scripts

---
+++ generated_test_scripts/sprint_1/test_case_2.py
@@ -0,0 +1,74 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+import sys
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+    
+    def login(self, username, password):
+        self.driver.find_element(By.XPATH, '//*[@id="user-name"]').send_keys(username)
+        time.sleep(3)
+        self.driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(password)
+        time.sleep(3)
+        self.driver.find_element(By.XPATH, '//*[@id="login-button"]').click()
+
+def test_payment_information_label_visibility():
+    options = Options()
+    options.add_argument("--incognito")
+    options.add_argument("--disable-notifications")
+    options.add_argument("--disable-popup")
+    options.add_argument("--disable-features=NetworkService")
+
+    driver = webdriver.Chrome(options=options)
+    
+    try:
+        driver.maximize_window()
+        driver.get("https://saucedemo.com/")
+        time.sleep(5)
+        
+        login_page = LoginPage(driver)
+        login_page.login("standard_user", "secret_sauce")
+        
+        driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-bike-light"]').click()
+        time.sleep(3)
+        
+        driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
+        time.sleep(3)
+        
+        driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a').click()
+        time.sleep(3)
+        
+        driver.find_element(By.XPATH, '//*[@id="checkout"]').click()
+        time.sleep(3)
+        
+        driver.find_element(By.XPATH, '//*[@id="first-name"]').send_keys("Jonnathan")
+        time.sleep(3)
+
+        driver.find_element(By.XPATH, '//*[@id="last-name"]').send_keys("K")
+        time.sleep(3)
+
+        driver.find_element(By.XPATH, '//*[@id="postal-code"]').send_keys("10007")
+        time.sleep(3)
+        
+        driver.find_element(By.XPATH, '//*[@id="continue"]').click()
+        time.sleep(3)
+        
+        payment_info_card = driver.find_element(By.XPATH, '//*[@id="checkout_summary_container"]/div/div[2]/div[1]')
+        assert payment_info_card.is_displayed(), "'Payment Information' label is not visible."
+        
+        driver.save_screenshot("C:\\Users\\Administrator\\Desktop\\QE_COE\\automated_pipeline_2\\captured_screenshots\\payment_info.png")
+        sys.exit(0)
+        
+    except Exception as e:
+        print(str(e))
+        driver.save_screenshot("C:\\Users\\Administrator\\Desktop\\QE_COE\\automated_pipeline_2\\captured_screenshots\\failed_case.png")
+        sys.exit(1)
+        
+    finally:
+        driver.quit()
+
+test_payment_information_label_visibility()
